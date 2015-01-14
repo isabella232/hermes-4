8ae20989c5c5c1980063fb25d29d05ae042c8b38
Marcello Barnaba <vjt@openssl.it>
2015-01-14 12:50:24 +0100

    Whitespace

--- app/models/concerns/path_scoping.rb
+++ app/models/concerns/path_scoping.rb
@@ -2,17 +2,19 @@ module PathScoping
   extend ActiveSupport::Concern
 
   included do
-case (adapter = ActiveRecord::Base.connection.class.name)
-when /PostgreSQLAdapter/
-  def self.within(path)
-    where("? ~* path_re", path.presence || '/')
-  end
-when /Mysql2Adapter/
-  def self.within(path)
-    where("? REGEXP path_re", path.presence || '/')
-  end
-else
-  raise "Your adapter #{ adapter.demodulize } is not supported yet"
-end
+    case (adapter = ActiveRecord::Base.connection.class.name)
+    when /PostgreSQLAdapter/
+      def self.within(path)
+        where("? ~* path_re", path.presence || '/')
+      end
+
+    when /Mysql2Adapter/
+      def self.within(path)
+        where("? REGEXP path_re", path.presence || '/')
+      end
+
+    else
+      raise "Your adapter #{ adapter.demodulize } is not supported yet"
+    end
   end
 end
