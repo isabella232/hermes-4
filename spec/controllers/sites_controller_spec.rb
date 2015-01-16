fa503bbda980ee039ff10360887308a813531c95
Lleïr Borràs Metje <lleir@llegeix.me>
2015-01-16 13:05:09 +0100

    Sites controller

--- spec/controllers/sites_controller_spec.rb
+++ spec/controllers/sites_controller_spec.rb
@@ -3,76 +3,139 @@
 require 'rails_helper'
 
 describe SitesController do
+  login
 
-  # TODO: auto-generated
+  let!(:site) { FactoryGirl.create :site }
+
   describe '#index' do
     it 'works' do
-      sites_controller = SitesController.new
-      result = sites_controller.index
-      expect(result).not_to be_nil
+      get :index
+
+      expect(assigns(:site)).to be_a_new Site
     end
   end
 
-  # TODO: auto-generated
   describe '#show' do
     it 'works' do
-      sites_controller = SitesController.new
-      result = sites_controller.show
-      expect(result).not_to be_nil
+      get :show, id: site.id
+
+      expect(assigns(:site)).to eq(site)
     end
   end
 
-  # TODO: auto-generated
   describe '#new' do
     it 'works' do
-      sites_controller = SitesController.new
-      result = sites_controller.new
-      expect(result).not_to be_nil
+      get :new
+
+      expect(assigns(:site)).to be_a_new Site
     end
   end
 
-  # TODO: auto-generated
   describe '#create' do
     it 'works' do
-      sites_controller = SitesController.new
-      result = sites_controller.create
-      expect(result).not_to be_nil
+      post :create, site: FactoryGirl.build(:site, user: nil).attributes
+
+      new_site = assigns(:site)
+
+      expect(new_site).to be_a Site
+      expect(new_site.user).to eq login_user
+    end
+
+    it 'increases the number of sites' do
+      expect{
+        post :create, site: FactoryGirl.build(:site, user: nil).attributes
+      }.to change(Site, :count).by(1)
+    end
+
+    it 'redirects on http' do
+      post :create, site: FactoryGirl.build(:site, user: nil).attributes
+
+      expect(response).to redirect_to(site_path(assigns(:site)))
+    end
+
+    it 'rennders in js' do
+      post :create, site: FactoryGirl.build(:site, user: nil).attributes, format: :js
+
+      expect(response).to have_http_status(:ok)
     end
   end
 
-  # TODO: auto-generated
   describe '#edit' do
-    it 'works' do
-      sites_controller = SitesController.new
-      result = sites_controller.edit
-      expect(result).not_to be_nil
+    it 'html' do
+      get :edit, id: site.id
+
+      expect(assigns(:site)).to eq(site)
+
+      expect(response).to render_template(:edit)
+    end
+
+    it 'js' do
+      xhr :get, :edit, id: site.id, format: :js
+
+      expect(assigns(:site)).to eq(site)
+
+      expect(response).to render_template(:edit)
     end
   end
 
-  # TODO: auto-generated
   describe '#update' do
     it 'works' do
-      sites_controller = SitesController.new
-      result = sites_controller.update
-      expect(result).not_to be_nil
+      new_site        = FactoryGirl.build(:site, protocol: 'https')
+      site_attributes = new_site.attributes
+
+      xhr :put, :update, id: site.id, site: site_attributes, format: :js
+
+      assigned_site = assigns(:site)
+
+      expect(assigned_site).to be_a Site
+      expect(assigned_site.attributes).not_to eq site.attributes
+
+      expect(assigned_site.name).to        eq new_site.name
+      expect(assigned_site.hostname).to    eq new_site.hostname
+      expect(assigned_site.description).to eq new_site.description
+      expect(assigned_site.protocol).to    eq new_site.protocol
+
+      expect(assigned_site.user_id).not_to eq new_site.user_id
+
+      expect(response).to render_template(:update)
     end
   end
 
-  # TODO: auto-generated
   describe '#destroy' do
     it 'works' do
-      sites_controller = SitesController.new
-      result = sites_controller.destroy
-      expect(result).not_to be_nil
+      xhr :delete, :destroy, id: site.id, format: :js
+
+      expect(assigns(:site)).not_to be_persisted
+
+      expect(response).to render_template(:destroy)
+    end
+
+    it 'decreases the number of sites' do
+      expect{
+        xhr :post, :destroy, id: site.id, format: :js
+      }.to change(Site, :count).by(-1)
     end
   end
 
-  # TODO: auto-generated
   describe '#general_broadcast' do
+    let!(:tip) { FactoryGirl.build :tip }
+
     it 'works' do
-      sites_controller = SitesController.new
-      result = sites_controller.general_broadcast
-      expect(result).not_to be_nil
+      xhr :post, :general_broadcast, tip: tip.attributes, format: :js
+
+      expect(assigns(:sites).to_a).to eq [site]
+      expect(assigns(:saved)).to      eq true
+
+      expect(response).to render_template(:general_broadcast)
+    end
+
+    it 'fails' do
+      xhr :post, :general_broadcast, tip: tip.attributes.merge(content: ''), format: :js
+
+      expect(assigns(:sites).to_a).to eq [site]
+      expect(assigns(:saved)).to      eq false
+
+      expect(response).to render_template(:general_broadcast)
     end
   end
 
--- spec/support/fake_authentication.rb
+++ spec/support/fake_authentication.rb
@@ -1,10 +1,11 @@
 module FakeAuthentication
   def login
+    let!(:login_user) { FactoryGirl.create :user }
+
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
 
-      user = FactoryGirl.create(:user)
-      sign_in user
+      sign_in login_user
     end
   end
 end
