bad65bc7217f7734a6a5f7948f19d2ee1f007911
Lleïr Borràs Metje <lleir@llegeix.me>
2015-01-21 12:56:07 +0100

    Fix missing site in tutorial_tips controller

--- app/controllers/tutorial_tips_controller.rb
+++ app/controllers/tutorial_tips_controller.rb
@@ -3,6 +3,13 @@ class TutorialTipsController < NestedTipsController
   load_resource :tutorial
   load_and_authorize_resource :tip, through: :tutorial, shallow: true, parent: false
 
+  before_filter :set_site
+
   self.nested_object = :@tutorial
+
+  protected
+    def set_site
+      @site = @tutorial.site
+    end
 
 end
