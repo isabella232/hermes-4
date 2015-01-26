dba729d49e0778b00e45986f4f8aaf73cd812225
Lleïr Borràs Metje <lleir@llegeix.me>
2015-01-26 10:58:19 +0100

    Finnish authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    can :manage, Tutorial, site_id: user.site_ids
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, Tutorial, site_id: user.site_ids
 
-    can [:manage, :general_broadcast], Site, user_id: user.id
-    can [:manage, :position], Tip
+      can [:manage, :general_broadcast], Site, user_id: user.id
+      can [:manage, :position], Tip, id: user.tip_ids
+    end
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :sites, inverse_of: :user, dependent: :destroy
+  has_many :tutorials, through: :sites
+
+  def tips
+    Tip.where(id: tip_ids)
+  end
+
+  def tip_ids
+    Tip.where(tippable: sites).pluck(:id) | \
+      Tip.where(tippable: tutorials).pluck(:id)
+  end
 
   def admin?
+    false #To be implemented
   end
 end
