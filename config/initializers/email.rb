477840edbe2595deee86e05b063a27c09a6062c0
Marcello Barnaba <vjt@openssl.it>
2015-02-24 17:14:57 +0100

    Fix stupidity :unamused:

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,6 +1,6 @@
 module Hermes
 
-  config = Rails.root.join('config', config)
+  config = Rails.root.join('config', 'email.yml')
   config = YAML.load_file(config).fetch(Rails.env)
   config.each do |k,v|
     v.symbolize_keys! if v.respond_to?(:symbolize_keys!)
