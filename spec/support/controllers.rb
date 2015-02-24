eb9c04f152f53e16aca84f85be6b7978f729a66c
Marcello Barnaba <vjt@openssl.it>
2015-02-24 13:55:32 +0100

    Fix loading issues when running `rake`

--- spec/support/controllers.rb
+++ spec/support/controllers.rb
@@ -1,4 +1,6 @@
 module Controllers
+  autoload :FakeAuthentication, 'support/controllers/fake_authentication.rb'
+
   RSpec.configure do |config|
     config.include Devise::TestHelpers, type: :controller
     config.extend  FakeAuthentication,  type: :controller
--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,4 +1,6 @@
 module Features
+  autoload :SessionHelpers, 'support/features/session_helpers'
+
   RSpec.configure do |config|
     config.include SessionHelpers, type: :feature
   end
