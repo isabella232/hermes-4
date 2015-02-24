3ab8698c7f0e41d6c83073d3cae8e71da75abfe0
Marcello Barnaba <vjt@openssl.it>
2015-02-24 13:38:50 +0100

    Add test email configuration

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,6 +1,7 @@
 module Hermes
-  config = Rails.root.join('config', 'email.yml')
 
+  config = Rails.env.test? ? 'email.test.yml' : 'email.yml'
+  config = Rails.root.join('config', config)
   config = YAML.load_file(config).fetch(Rails.env)
   config.each do |k,v|
     v.symbolize_keys! if v.respond_to?(:symbolize_keys!)
